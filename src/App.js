8c033aeb91461d059fcf6bb3de103fb6824606c5
Dahan Keshala <dahankesh@icloud.com>
2024-08-08 20:50:29 +0530

    Create branch

--- src/App.js
+++ src/App.js
@@ -4,43 +4,7 @@ import TodoLIts from "./components/TodoLIts";
 import "./App.css";
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (text) => {
-    const newTodo = {
-      id: Date.now(),
-      text,
-      completed: false,
-    };
-
-    setTodos([newTodo, ...todos]);
-    console.log(todos);
-  };
-
-  const toggleComplete = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-  return (
-    <>
-      <div className="app">
-        <h1>Todo List</h1>
-        <TodoForm addTodo={addTodo} />
-        <TodoLIts
-          todos={todos}
-          toggleComplete={toggleComplete}
-          deleteTodo={deleteTodo}
-        />
-      </div>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
